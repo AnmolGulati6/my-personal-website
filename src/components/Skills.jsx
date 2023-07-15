53e5aca9d0880e715440bcc785c2b313bbf5daac
Anmol Gulati <61374198+AnmolGulati6@users.noreply.github.com>
2023-07-15 15:58:02 -0700

    Initial commit

---
+++ src/components/Skills.jsx
@@ -0,0 +1,63 @@
+
+
+import Python from '../assets/python.png';
+import Java from '../assets/java.png';
+import Swift from '../assets/swift.png';
+import C from '../assets/c.png';
+import JavaScript from '../assets/javascript.png';
+import React from '../assets/react.png';
+import HTML from '../assets/html.png';
+import CSS from '../assets/css.png';
+
+
+
+const Skills = () => {
+  return (
+    <div name='skills' className='w-full h-screen bg-[#0a192f] text-gray-300'>
+      {/* Container */}
+      <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
+          <div>
+              <p className='text-4xl font-bold inline border-b-4 border-pink-600 '>Skills</p>
+              <p className='py-4'>Here's a preview of the skills I've picked up on my programming journey over the past few years</p>
+          </div>
+
+          <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8'>
+              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                  <img className='w-20 mx-auto' src={Python} alt="HTML icon" />
+                  <p className='my-4'>Python</p>
+              </div>
+              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                  <img className='w-20 mx-auto' src={Java} alt="HTML icon" />
+                  <p className='my-4'>Java</p>
+              </div>
+              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                  <img className='w-20 mx-auto' src={Swift} alt="HTML icon" />
+                  <p className='my-4'>Swift</p>
+              </div>
+              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                  <img className='w-20 mx-auto' src={C} alt="HTML icon" />
+                  <p className='my-4'>C/C++</p>
+              </div>
+              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                  <img className='w-20 mx-auto' src={JavaScript} alt="HTML icon" />
+                  <p className='my-4'>JavaScript</p>
+              </div>
+              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                  <img className='w-20 mx-auto' src={React} alt="HTML icon" />
+                  <p className='my-4'>React</p>
+              </div>
+              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                  <img className='w-20 mx-auto' src={HTML} alt="HTML icon" />
+                  <p className='my-4'>HTML</p>
+              </div>
+              <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                  <img className='w-20 mx-auto' src={CSS} alt="HTML icon" />
+                  <p className='my-4'>CSS</p>
+              </div>
+          </div>
+      </div>
+    </div>
+  );
+};
+
+export default Skills;
